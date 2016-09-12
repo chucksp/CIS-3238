6ceb2622bb2794b87b8daf7ef5a1ab1ce5c6f7fa
chucksp <charles.s.patterson@gmail.com>
2016-09-12 15:33:38 -0400

    Add files via upload

--- cis3238lab1/src/cis3238lab1/TestJunit.java
+++ cis3238lab1/src/cis3238lab1/TestJunit.java
@@ -17,11 +17,35 @@ public class TestJunit {
     Date d1 = new Date(2001, 7, 15);
     Date d2 = new Date(1991, 1, 23);
     Date d3 = new Date(2009, 5, 9);
+    Date d4 = new Date(2006, 9, 2);
     
     @Test    
-    public void testPrintMessage() {
+    public void testPrintMessage1() {
         assertEquals(3838, d1.daysTo(d2));
-        assertEquals(2986, d1.daysTo(d3));
+    }
+    
+    @Test    
+    public void testPrintMessage2() {
+        assertEquals(1, d1.daysTo(d3));
+    }
+    
+    @Test    
+    public void testPrintMessage3() {
         assertEquals(6704, d2.daysTo(d3));
+    }
+    
+    @Test    
+    public void testPrintMessage4() {
+        assertEquals(20, d4.daysTo(d1));
+    }
+    
+    @Test    
+    public void testPrintMessage5() {
+        assertEquals(5736, d4.daysTo(d2));
+    }
+    
+    @Test    
+    public void testPrintMessage6() {
+        assertEquals(20, d4.daysTo(d3));
     }
 }
